1d9539669dcc9b6e87210225c465b0e0de7639d9
Nelson <nsandrad20@gmail.com>
2025-03-17 21:06:28 -0700

    (NA) Feature-C: Functionality for survey

--- welcome.js
+++ welcome.js
@@ -1,14 +1,17 @@
-
-// welcome.js
-import {validateAge} from "./validateAge.js";
+import { validateAge } from "./validateAge.js";
 import { verifyName } from './getName.js';
-
-const age= validateAge(age);
+import { deliverSurvey } from "./survey.js";
 
 console.log("=".repeat(35));
 console.log("Welcome to eligibility check");
 console.log("=".repeat(35));
 console.log("\n");
+
+const age = validateAge(age);
 const userName = verifyName(username);
 console.log(`Hello ${userName}!\n`);
+
+const survey_flag = prompt("Would you like to participate in our survey (Y/N): ")
+if(survey_flag == 'Y')
+deliverSurvey()
 
